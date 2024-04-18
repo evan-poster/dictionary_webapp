9d104a8223474a072de4e0a77799ce5712fc66c9
evan-poster <evan.poster@gmail.com>
2024-04-18 11:09:58 -0400

    words.models: added Source, Word, Definition models

--- django/words/models.py
+++ django/words/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# Source model
+class Source():
+    # Fields
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+
+    # Methods
+    def __str__(self):
+        return self.name
+
+
+# Words model
+class Word(models.Model):
+    # Fields
+    word = models.CharField(max_length=255)
+
+    # Methods
+    def __str__(self):
+        return self.word
+
+
+class Definition(models.Model):
+    # Fields
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    definition = models.TextField()
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+
+    # Methods
+    def __str__(self):
+        return self.definition
