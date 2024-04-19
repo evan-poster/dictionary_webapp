90b6c44d34805f4ed7e933ac5a569f189635ef76
evan-poster <evan.poster@gmail.com>
2024-04-19 14:16:23 -0400

    +layout.svelte added

---
+++ svelte-project/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<div class="ui main text container">
+	<slot />
+</div>
+
+<style>
+	.ui.main.text.container {
+		margin-top: 5em;
+	}
+</style>
