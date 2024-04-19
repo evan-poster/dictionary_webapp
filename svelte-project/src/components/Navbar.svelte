8954e204445f1a5efa3613a6e75dec0ade15ec57
evan-poster <evan.poster@gmail.com>
2024-04-19 14:16:34 -0400

    +Navbar.svelte added to components

---
+++ svelte-project/src/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+	let Page;
+	$: Page = $page.url.pathname;
+</script>
+
+<div class="ui fixed inverted menu">
+	<div class="ui container">
+		<a href="/" class="header item">
+			<img class="logo" src="assets/images/logo.png" alt="Dictionary Logo"/>
+			Dictionary
+		</a>
+		<a href="/" class={Page === '/' ? 'active item' : 'item'}>Home</a>
+		<a href="/lookup" class={Page === '/lookup' ? 'active item' : 'item'}>Lookup</a>
+		<a href="/about" class={Page === '/about' ? 'active item' : 'item'}>About</a>
+	</div>
+</div>
