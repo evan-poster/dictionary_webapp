d32b2d64d87d15921a98bdbfaccf82dfc0d30529
evan-poster <evan.poster@gmail.com>
2024-04-22 10:30:36 -0400

    word.views.word_detail added

--- django-project/words/views.py
+++ django-project/words/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+# JSON Response for a word
+
+from django.http import JsonResponse
+from .models import Word
+
+
+def word_detail(request, word_text):
+    """
+    View for fetching a word
+    """
+    try:
+        word = Word.objects.get(word=word_text)
+    except Word.DoesNotExist:
+        return JsonResponse({'error': 'Word does not exist'}, status=404)
+
+    data = {
+        'word': word.word,
+        'definitions': [defn.definition for defn in word.definitions.all()],
+        'part_of_speeches': [defn.part_of_speech for defn in word.definitions.all()],
+    }
+
+    return JsonResponse(data)
